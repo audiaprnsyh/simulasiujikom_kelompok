2ccbb7619da9996cc7b2dfbba449f4ef30a4adbd
Fahripurwana23 <muhamadery2324@gmail.com>
2025-02-07 09:58:53 +0700

    perubahahan migration typo

--- database/migrations/2025_02_07_022514_create_peminjaman_table.php
+++ database/migrations/2025_02_07_022514_create_peminjaman_table.php
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('peminjamen');
+        Schema::dropIfExists('peminjaman');
     }
 };
